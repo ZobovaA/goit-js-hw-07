74c3f1ddb20c98ae714b3979a8fdec03e1c127af
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-04-28 20:02:59 +0300

    1 task done

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,22 @@
+const categories = document.querySelector('ul#categories')
+const elements = categories.children
+
+// Задание 1 
+const elementsNum = elements.length
+console.log(`В списке ${elementsNum} категории.`)
+
+// Задание 2
+for(const element of elements){
+    const heading = element.children[0].textContent
+   
+    console.log(`Категория: ${heading}`)
+
+    console.log(`Количество элементов:${element.querySelectorAll('li').length}`)
+
+}
+
+
+
+
+
 
