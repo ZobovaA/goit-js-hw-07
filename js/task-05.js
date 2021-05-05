40ea2c004ce4e7c0c46acd4201a398234fe89ded
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-05-05 21:28:22 +0300

    5 исправила

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,10 @@
 const inputEl = document.querySelector("input");
 const ouputEl = document.querySelector("#name-output");
-const typeName = (() => {
-    if (inputEl.value.length >=1) {
-        return ouputEl.textContent = inputEl.value;   
-    }
-    else ouputEl.textContent = 'незнакомец';
-})
-inputEl.addEventListener('input', typeName)
+const typeName = () => {
+  if (inputEl.value.length >= 1) {
+  (ouputEl.textContent = inputEl.value);
+  } else {
+    ouputEl.textContent = "незнакомец";
+  }
+};
+inputEl.addEventListener("input", typeName);
