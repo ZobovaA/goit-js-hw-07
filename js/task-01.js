f4d7aa71989a4eea52aa3653ba55524e9bdf58af
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-05-06 15:27:19 +0300

    переделала 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,11 @@
-const categories = document.querySelector('ul#categories')
-const elements = categories.children
-const elementsNum = elements.length
-console.log(`В списке ${elementsNum} категории.`)
-
-for(const element of elements){
-    const heading = element.children[0].textContent
-   
-    console.log(`Категория: ${heading}`)
-
-    console.log(`Количество элементов:${element.querySelectorAll('li').length}`)
-
-}
-
-
-
-
-
-
+const categories = document.querySelector("ul#categories");
+const elements = categories.children;
+const elementsNum = elements.length;
+console.log(`В списке ${elementsNum} категории.`);
+const newArr = [...elements];
+newArr.forEach((element) => {
+  const findTitle = document.querySelector("h2");
+  const heading = findTitle.textContent;
+  console.log(`Категория: ${heading}`);
+  console.log(`Количество элементов:${element.querySelectorAll("li").length}`);
+});
